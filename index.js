7d353f6504fbc3cf4a845a8d6189ea348da970d0
Synox <synox@users.noreply.github.com>
2016-12-04 13:03:51 +0100

    reduce scripts

--- index.js
+++ index.js
@@ -3,81 +3,64 @@ var app = angular.module('app', []);
 
 var snd = new Audio("click-short.wav");
 
-app.controller('MainCtrl', ["$interval", "$timeout", function ($interval, $timeout) {
+app.controller('MainCtrl', ["$interval", function ($interval) {
     var self = this;
-
 
     self.onReset = function () {
         self.mode = "init";
         self.lastPress = null;
         self.interval = null;
-        self.timeToNextBeat = null;
         $interval.cancel(self.beatTimer);
         self.beatTimer = null;
         self.timeSinceLastPress = null;
+        self.progressPercent = 0;
     };
 
     self.updateUiVariables = function () {
-        if (self.lastPress) {
+        if (self.mode == "init") {
+            return;
+        } else if (self.mode == "measure") {
             self.timeSinceLastPress = new Date().getTime() - self.lastPress
-        }
-        if (self.interval) {
-            self.timeToNextBeat = self.nextBeatTime - new Date().getTime();
-            // self.progressPercent = 100.0 / self.interval * (self.interval- self.timeToNextBeat);
-            self.progressPercent = 100.0 - 100.0 / self.interval * self.timeToNextBeat;
-        } else {
-            self.timeToNextBeat = null;
-            self.progressPercent = null;
+
+        } else if (self.mode == "play") {
+            var timeToNextBeat = self.nextBeatTime - new Date().getTime();
+            self.progressPercent = 100.0 - 100.0 / self.interval * timeToNextBeat;
         }
     }
-
-    self.getTimeToNextBeat = function () {
-        console.log("getTimeToNextBeat");
-        return;
-    };
-
-    self.onStart = function () {
-        var now = new Date().getTime();
-        self.mode = "measure";
-        self.lastPress = now;
-
-        self.onBeat();
-    };
-
-    self.onMeasure = function () {
-
-        var now = new Date().getTime();
-        self.mode = "play";
-        self.interval = now - self.lastPress;
-        self.lastPress = now;
-
-        self.onBeat();
-
-        if (self.beatTimer) {
-            $interval.cancel(self.beatTimer);
-        }
-        if (self.interval) {
-            self.beatTimer = $interval(function () {
-                self.onBeat();
-            }, self.interval);
-
-        }
-
-    };
 
     self.onBeat = function () {
         snd.play();
         self.nextBeatTime = new Date().getTime() + self.interval;
     };
 
+    self.onStart = function () {
+        var now = new Date().getTime();
+        self.lastPress = now;
+
+        self.onBeat();
+        self.mode = "measure";
+    };
+
+    self.onMeasure = function () {
+        var now = new Date().getTime();
+        self.interval = now - self.lastPress;
+        self.lastPress = now;
+
+        $interval.cancel(self.beatTimer);
+        self.beatTimer = $interval(function () {
+            self.onBeat();
+        }, self.interval);
+
+        self.onBeat();
+        self.mode = "play";
+
+    };
 
     self.intervalPromise = $interval(function () {
         self.updateUiVariables();
-    }, 33);
+    }, 100);
 
 
     self.onReset();
 
-
-}])
-;
+}]);
